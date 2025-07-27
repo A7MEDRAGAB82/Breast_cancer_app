ec550657addae0b68e102f240cf6b21816b547e0
Ahmed Ragab <sci.AhmedRagab85728@alexu.edu.eg>
2025-07-27 14:11:00 +0300

    Train models and evaluate performance in app

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ from model import get_models, train_and_evaluate, get_feature_importance
 from utils import plot_prediction_bar, plot_feature_importance, show_confusion_matrices
 
 df = load_data()
+
+models = get_models()
+results = train_and_evaluate(models, df)
