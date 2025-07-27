e94d356ff41a17046c9ebc12a1ebe1e635e478a8
Ahmed Ragab <sci.AhmedRagab85728@alexu.edu.eg>
2025-07-27 13:52:16 +0300

    Add bar plot to show feature importance

--- utils.py
+++ utils.py
@@ -9,3 +9,14 @@ def plot_prediction_bar(proba):
     ax.set_xlim(0, 1)
     ax.set_title('Prediction Confidence')
     st.pyplot(fig)
+
+def plot_feature_importance(features, importances):
+    importance_df = pd.DataFrame({
+        'Feature': features,
+        'Importance': importances
+    }).sort_values('Importance', ascending=False)
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.barplot(data=importance_df, x='Importance', y='Feature', ax=ax, palette='viridis')
+    ax.set_title('Feature Importance for Prediction')
+    st.pyplot(fig)
